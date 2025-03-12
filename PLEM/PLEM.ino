84eb247125c80236977351fac368d55edc9b06c5
Victor de Moura <victordemoura0409@gmail.com>
2025-03-12 11:02:49 -0300

    1.1

--- PLEM/PLEM.ino
+++ PLEM/PLEM.ino
@@ -1,31 +1,35 @@
 //Bibilotecas
-#include <Wire.h>                 //Biblioteca para I2C
-#include <LiquidCrystal_I2C.h>    //Biblioteca da LCD
-#include <SD.h>                   //Biblioteca do SD
-#include <SPI.h>                  //
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <SD.h>
+#include <SPI.h>
 
+//Pino do Esp32
 
 //Variaveis de controle de tempo
 unsigned long tempsensor = 11000;
 unsigned long templcd = 6000; 
 
-//Variaveis Globais
+//Obejtos de Classes
 File dataFile;
 LiquidCrystal_I2C lcd(0x27,16,2);
+
+//Variaveis Globais
 int co2ppm = 0; //verificar se coloca no tipo float
 int estadoLCD = 0;
 
-void setup() {
-  
+void setup() { 
+
 //Inicializa o Display LCD
 lcd.init();
+lcd.backlight();
  
 //Módulo Micro-SD
 
 // Tenta Inicializar o cartão SD
 if(!SD.begin(5)) {
     Cursor(0,0,1);
-    lcd.print("Falha ao inicializar o cartão SD!");
+    lcd.print("Falha no SD!");
     while(1);
   } 
 
@@ -40,8 +44,8 @@ if (dataFile) {
 } else {
     Cursor(0,0,1);
     lcd.print("Erro no arquivo!");
+    while(1);
 }
- /////////////////////////////////////////////////////////////////////
 
 }
 
@@ -53,18 +57,37 @@ void loop() {
     Salva_sd();
     tempsensor = millis();
   }
-  
+   
 //controla a taxa de tempo do Display
  if (millis() - templcd >= 5000) { 
     Display();
     templcd = millis();     
   }
- 
 }
+
+/////////////////////////////////////////////////////////////////////
+//Instanciandos as funções dos Módulos/Sensores
 
 //Micro_SD
 void Salva_sd(){
-dataFile.println(String(co2ppm)+";");
+dataFile.print("-;");//Dia
+dataFile.print("-;");//Mês
+dataFile.print("-;");//Ano
+dataFile.print("-;");//Hora
+dataFile.print("-;");//Minuto
+dataFile.print("-;");//Segundo
+dataFile.print("-;");//Latitude
+dataFile.print("-;");//Longitudo
+dataFile.print("-;");//Velocidade(km/h)
+dataFile.print("-;");//Satélite
+dataFile.print("-;");//Temperatura do ar
+dataFile.print("-;");//Umidade do ar
+dataFile.print("-;");//Temperatura de globo(C)
+dataFile.print("-;");//Velocidade do vento(m/s)
+dataFile.print(String(co2ppm)+";");//Concentração de CO2
+dataFile.print("-;");//Iluminancia
+dataFile.println("-");//Ruido Ambiente
+
 dataFile.flush(); // Garante que os dados sejam gravados no cartão SD
 }
 
@@ -75,21 +98,15 @@ if(estadoLCD == 0){
     lcd.print("CO2 ambiente: "); 
     Cursor(0,1,0); 
     lcd.print(String(co2ppm)+"ppm");  
-}
+  }
 estadoLCD++;
-if(estadoLCD>=0){
-    estadoLCD = 0;
-}
-
+if(estadoLCD>=0){estadoLCD = 0;}
 }
 
 //Posiciona o Cursor do Diplay LCD
 void Cursor(int linha,int coluna,bool limpar){
-if (limpar){ 
-    lcd.clear(); //limpa o display se limpar for 1
-}
+if (limpar){ lcd.clear();}//limpa o display se limpar for 1
 lcd.setCursor(linha,coluna);
-
 }
 
 //Sensor CO2
@@ -97,8 +114,7 @@ void Co2(){
 int co2soma = 0;
 for(int x; x<10;x++){
     co2soma += analogRead(35);
-    delay(100); //Verificar como que vai afetar o código
+    delay(100);
   }
-co2ppm = (co2soma/10) - 350;
-
+co2ppm = (co2soma/10) + 350;//Verificar se é +/- 
 }
